e1f6a014aab0c6165e8305a988c00cc929bbc30b
Nadeem Hossain <nadim.sarker461@gmail.com>
2021-08-12 20:44:50 +0600

    init

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+     int a;
+    printf("Enter the number you want multiplication table of : \n");
+    scanf("%d",&a);
+    for (int j = 1; j <= a; j++)
+    {printf("\nMultiplication table of %d\n\n",j);
+         for ( int i = 1; i <=10; i++)
+    {
+        printf("%d x %d = %d\n",j,i,j*i);
+    }
+    }
+    return 0;
+    }
+    
+   
