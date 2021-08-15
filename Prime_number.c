3c2f95d10030fd53606542a0294d3fc6717244ee
Nadeem Hossain <nadim.sarker461@gmail.com>
2021-08-15 16:08:35 +0600

    init

--- Prime_number.c
+++ Prime_number.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 int main(){
-    printf("Hello");
+    int  number,flag=0;
+    printf("Enter a number : ");
+    scanf("%d",&number);
+    for (int i = 2; i < number/2; i++)
+    {
+        if (number%i==0)
+        {
+            printf("%d is not a prime number",number);
+            flag=1;
+            break;
+        }
+    }
+    if (flag==0)
+    {
+        printf("%d is a prime number", number);
+    }
+    
+
     return 0;
 }
