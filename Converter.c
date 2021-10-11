b4eddaa84a898a873f6c045c4e52fc90987d889b
Nadeem Hossain <nadim.sarker461@gmail.com>
2021-10-11 18:35:39 +0600

    Chemistry add korsi

--- Converter.c
+++ Converter.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int select;
-    printf("1. Length\n2. Area\n3. Volume\n4. Mass\n5. Velocity\n6. Pressure\n7. Energy\n8. Power\n9. Temperature\n\n Select: ");
+    printf("1. Length\n2. Area\n3. Volume\n4. Mass\n5. Velocity\n6. Pressure\n7. Energy\n8. Power\n9. Temperature\n10. Chemistry\n\n Select: ");
     scanf("%d",&select);
 
 
@@ -14,7 +14,7 @@ int main(){
     if (num==1)
     {
         int no_1;
-        printf("\nEnter your number:");
+        printf("\nEnter the quantity:");
         scanf("%d",&no_1);
         float No_1= no_1*2.54;
         printf("%d inch = %.3f cm",no_1,No_1);
@@ -23,7 +23,7 @@ int main(){
     else if (num==2)
     {
         int no_2;
-        printf("\nEnter your number:");
+        printf("\nEnter the quantity:");
         scanf("%d",&no_2);
         float No_2= no_2/2.54;
         printf("%d cm = %.3f inch",no_2,No_2);
@@ -32,7 +32,7 @@ int main(){
     else if (num==3)
     {
         int no_3;
-        printf("\nEnter your number:");
+        printf("\nEnter the quantity:");
         scanf("%d",&no_3);
         float No_3= no_3/3.281;
         printf("%d ft = %.3f m",no_3,No_3);
@@ -41,7 +41,7 @@ int main(){
     else if (num==4)
     {
         int no_4;
-        printf("\nEnter your number:");
+        printf("\nEnter the quantity:");
         scanf("%d",&no_4);
         float No_4= no_4*3.281;
         printf("%d m = %.3f ft",no_4,No_4);
@@ -50,7 +50,7 @@ int main(){
     else if (num==5)
     {
         int no_5;
-        printf("\nEnter your number:");
+        printf("\nEnter the quantity:");
         scanf("%d",&no_5);
         float No_5= no_5*1.609;
         printf("%d mile = %.3f km",no_5,No_5);
@@ -59,7 +59,7 @@ int main(){
     else if (num==6)
     {
         int no_6;
-        printf("\nEnter your number:");
+        printf("\nEnter the quantity:");
         scanf("%d",&no_6);
         float No_6= no_6/1.609;
         printf("%d km = %.3f mile",no_6,No_6);
