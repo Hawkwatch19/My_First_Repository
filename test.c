d3990390196d745c1cb82a12894be5efb6447e3f
Nadeem Hossain <nadim.sarker461@gmail.com>
2021-10-16 01:40:38 +0600

    ij

--- test.c
+++ test.c
@@ -1,18 +1,30 @@
 #include<stdio.h>
 int main(){
-    char j= '*' ;
-  for (int i = 1; i <= 4; i++)
-  {
-      for ( j = 1; j <= 5; j++)
-      {
-          printf("%d", j);
-      }
-      printf("\n");
-  }
-  
-  
 
-  
-   
+    int no_Of_Rows;
+    printf("Number of Rows to be printed \n");
+    scanf("%d",&no_Of_Rows);
+
+    int row,colSpaces,colChar;
+    for (row = 0; row < no_Of_Rows; row++)
+    {
+        for ( colSpaces = no_Of_Rows-row; colSpaces >= 1; colSpaces--)
+        {
+            printf(" ");
+        }
+        
+        int printChar=1;
+        int  num=row;
+        int den=1;
+        for (colChar = 0; colChar <= row; colChar++)
+        {
+        printf("%d ",printChar);
+        printChar=printChar*num;
+        printChar=printChar/den;
+        num--;
+        den++;
+        }
+    printf("\n");
+    }
     return 0;
 }
