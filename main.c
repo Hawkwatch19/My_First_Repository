718adee2271a6ee8646b67bbeb167cd6dd9047dd
Nadeem Hossain <nadim.sarker461@gmail.com>
2021-10-16 01:39:13 +0600

    kd

--- main.c
+++ main.c
@@ -3,9 +3,9 @@ int main(){
   int j;
   for (int i = 1; i <= 4; i++)
   {
-      for ( j = 1; j <= 5; j++)
+      for ( j = 1; j <= i; j++)
       {
-          printf("%d", j);
+          printf("%d",i);
       }
       printf("\n");
   }
--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-  int j;
+    char j= '*' ;
   for (int i = 1; i <= 4; i++)
   {
       for ( j = 1; j <= 5; j++)
