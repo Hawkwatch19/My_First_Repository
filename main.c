22adceee11166aacc1ecd5692221c84756e030c9
Nadeem Hossain <nadim.sarker461@gmail.com>
2021-10-16 01:19:42 +0600

    d

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 int main(){
+  int j;
+  for (int i = 1; i <= 4; i++)
+  {
+      for ( j = 1; j <= 5; j++)
+      {
+          printf("%d", j);
+      }
+      
+  }
+  
+  
+
+  
    
     return 0;
 }
